39357ec5b7340a6bdec0ab0dcbdd22966113697e
Erik Aylward <eaylward@mail.med.upenn.edu>
2019-01-21 18:37:06 -0500

    Remove code

--- classes/closest_zip_finder.rb
+++ classes/closest_zip_finder.rb
@@ -7,17 +7,4 @@ class ClosestZipFinder
   def closest_zip(zipcodes, lat_long)
     zipcodes.min_by { |zip| DistanceFinder.new(MyZipCodeFinder.geolocate(zip), lat_long).distance }
   end
-
-  # original implementation:
-  # def get_zip(a, location)
-  #   acc = {}
-  #   a.each do |zipcode|
-  #     lat = MyZipCodeFinder.geolocate(zipcode)[0]
-  #     long = MyZipCodeFinder.geolocate(zipcode)[1]
-  #     distance = DistanceFinder([lat,long], [location[0], location[1]])
-  #     acc['zipcode'] = distance
-  #   end
-  #   zipcode_of_min_distance = acc.min_by { |_, v| v }[0]
-  #   zipcode_of_min_distance
-  # end
 end
